384bf7014f8b55c7499b8b55518c57b48abe75be
Pinaak510 <105143140+Pinaak510@users.noreply.github.com>
2022-05-09 22:15:42 +0530

    Update streamlit_app.py
    added requests lib to fetch data from API

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit, pandas
+import streamlit, pandas, requests
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -20,3 +20,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits: ",list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruits/watermelon")
+
+streamlit.text(fruityvice_response)
