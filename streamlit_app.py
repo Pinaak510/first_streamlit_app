63465799aea12157edaf0da6746ff486a5ec0762
Pinaak510 <105143140+Pinaak510@users.noreply.github.com>
2022-05-24 15:11:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,9 +41,9 @@ my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 
 my_data_row = my_cur.fetchone()
 
-streamlit.text('The fruit load list contains:')
+streamlit.header('The fruit load list contains:')
 
-streamlit.text(my_data_row)
+streamlit.dataframe(my_data_row)
 
 
 
